3be2f60ff3f3934a5f88fb5d930c582d5fe63463
Prasanth Madhaiyan <prasanth.madhaiyan@syncfusion.com>
2022-12-28 19:39:17 +0530

    feature(EJ2-66059): Updated Web Mail sample netcoreapp2.0 to net6.0

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Outlook_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return PartialView("home");
+        }
+        public IActionResult NewMail()
+        {
+            return PartialView("newmail");
+        }
+        public ActionResult Readingpane()
+        {
+            return PartialView("readingpane");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Outlook_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
